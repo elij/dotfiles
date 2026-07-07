961497ff04be00e841984731d0a4911c6f2ed859
Elijah Charles <elij@lypy.com>
2026-07-07 16:08:28 +0100

    chore: switched to ratex fork

--- .config/emacs/lisp/setup-ratex.el
+++ .config/emacs/lisp/setup-ratex.el
@@ -1,23 +1,14 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package ratex
-  :ensure nil
-  :load-path "lisp/"
+  :vc (:url "https://github.com/elij/ratex.el" 
+            :lisp-dir "lisp")
   :after exec-path-from-shell
   :defer t
-  :preface
-  (defun my-ratex-add-dollar-delimiters-advice (orig-fun &rest args)
-    (let ((ratex--delimiter-pairs (append '(("$$" . "$$") ("$" . "$")) ratex--delimiter-pairs)))
-      (apply orig-fun args)))
-
   :config
-  (setq ratex-dark-render-color "white")
-  (setq ratex-light-render-color "black")
-  (advice-add 'ratex-fragments-in-buffer :around #'my-ratex-add-dollar-delimiters-advice)
-  (advice-add 'ratex-fragment-at-point :around #'my-ratex-add-dollar-delimiters-advice)
-  :hook ((markdown-mode . global-ratex-mode))
-  :custom
-  (ratex-backend-binary "ratex-editor-backend"))
-
+  (setq ratex-dark-render-color "white"
+        ratex-light-render-color "black")
+  :hook ((markdown-mode . global-ratex-mode)
+         (markdown-ts-mode . global-ratex-mode)))
 
 (provide 'setup-ratex)
