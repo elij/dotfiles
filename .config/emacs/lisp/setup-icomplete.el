92c43055fe743f49b0f52e67482a72926f482c51
Elijah Charles <elij@lypy.com>
2026-07-16 09:35:49 +0100

    fix: force icomplete to one frame
    - overlay only visible on active frame

--- .config/emacs/lisp/setup-icomplete.el
+++ .config/emacs/lisp/setup-icomplete.el
@@ -1,7 +1,14 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package icomplete
+  :ensure nil
   :defer t
-  :hook (after-init . fido-vertical-mode))
+  :hook (after-init . fido-vertical-mode)
+  :config
+  (advice-add 'icomplete-exhibit :after
+              (lambda (&rest _)
+                (when (and (window-minibuffer-p) 
+                           (overlayp icomplete-overlay))
+                  (overlay-put icomplete-overlay 'window (selected-window))))))
 
 (provide 'setup-icomplete)
