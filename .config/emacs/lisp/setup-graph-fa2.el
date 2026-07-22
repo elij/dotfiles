68b5f3c1295990815cb533a62af8f304e4c5f311
Elijah Charles <elij@lypy.com>
2026-07-22 16:39:13 +0100

    chore: graph-fa2 tweaks

--- .config/emacs/lisp/setup-graph-fa2.el
+++ .config/emacs/lisp/setup-graph-fa2.el
@@ -4,6 +4,9 @@
   :ensure nil
   :vc (:url "https://github.com/elij/graph-fa2")
   :defer t
+  :config
+  (setq graph-fa2-surface-constraint 'none)
+  (setq graph-fa2-horizon-threshold 131768)
   :commands (graph-fa2-start graph-fa2-render))
 
 (provide 'setup-graph-fa2)
